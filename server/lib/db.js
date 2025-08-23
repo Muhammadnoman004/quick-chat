b6ddd6a4dcd9e376bf32eb9814cdbfc5021b20cb
Noman Shamim <nomanshamim2004@gmail.com>
2025-08-23 23:51:29 +0500

    backend setup & DB connected.

---
+++ server/lib/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+// Function to connect to the mongodb Database
+
+export const connectDB = async () => {
+    try {
+        mongoose.connection.on('connected', () => console.log("Database connected."));
+        await mongoose.connect(`${process.env.MONGODB_URI}/chat-app`)
+    } catch (error) {
+        console.log("DB connection error --->", error);
+    }
+}
---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv/config";
+import cors from "cors";
+import http from "http";
+
+import { connectDB } from "./lib/db.js";
+
+// Create Express app and HTTP server
+
+const app = express();
+const server = http.createServer(app);  // The socket io support this http server
+
+// Middleware setup
+
+app.use(express.json({ limit: "4mb" }));
+app.use(cors());
+
+app.use("/api/status", (req, res) => res.send("Server is live"));
+
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
+
+// connect to MongoDB
+await connectDB();

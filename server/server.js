c06f56d74115e116f86f0626687a16e0b48fbf9f
Noman Shamim <nomanshamim2004@gmail.com>
2025-09-13 23:53:30 +0500

    finally backend  completed.

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv/config";
 import cors from "cors";
 import http from "http";
+import { Server } from "socket.io";
 
 import { connectDB } from "./lib/db.js";
 import userRouter from "./routes/userRoutes.js";
@@ -11,6 +12,36 @@ import messageRouter from "./routes/messageRoutes.js";
 
 const app = express();
 const server = http.createServer(app);  // The socket io support this http server
+
+//Initialize socket.io server
+
+export const io = new Server(server, {
+    cors: { origin: "*" }
+})
+
+// Store online users
+
+export const userSocketMap = {};  // {userId: socketId}
+
+// socket.io connection handler
+
+io.on("connection", (socket) => {
+    const userId = socket.handshake.query.userId;
+    console.log("User Connected", userId);
+
+    if (userId) userSocketMap[userId] = socket.id;
+
+    // Emit Online users to all connected clients
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+    socket.on("disconnect", () => {
+        console.log("User Disconnected", userId);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+    })
+
+})
 
 // Middleware setup
 
